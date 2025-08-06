26e011725c982dc7d821429e27fa0f7d12c5ff88
TTjirakit <captain112006@gmail.com>
2025-08-06 16:18:35 +0700

    done

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -45,11 +45,14 @@ function App() {
     setTasks(newTasks);
   };
 
+  const totalTasks = tasks.length;
+  const completeTask = tasks.filter((task) => task.isDone).length;
+
   return (
     <div className="col-12 m-2 p-0">
       <div className="container text-center">
         <h2>Todo List</h2>
-        <span className="m-2">All : () Done : ()</span>
+        <span className="m-2">All : ({totalTasks}) Done : ({completeTask})</span>
         {/* Modal Component */}
         <button
           type="button"
